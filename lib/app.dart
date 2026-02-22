ee763a74a2bdba0fabfc074c3b469db83df26518
Amitsaka1 <111147203+Amitsaka1@users.noreply.github.com>
2026-02-23 02:32:43 +0530

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+
 import 'package:secure_subscription_app/shared/layout/app_layout.dart';
 import 'package:secure_subscription_app/features/auth/presentation/login_screen.dart';
-import 'package:secure_subscription_app/features/chat/presentation/chat_screen.dart';
 import 'package:secure_subscription_app/features/dashboard/presentation/dashboard_screen.dart';
+import 'package:secure_subscription_app/features/chat/presentation/chat_list_screen.dart';
+import 'package:secure_subscription_app/features/chat/presentation/chat_conversation_screen.dart';
+import 'package:secure_subscription_app/features/profile/presentation/create_profile_screen.dart';
+import 'package:secure_subscription_app/features/profile/presentation/profile_details_screen.dart';
 import 'package:secure_subscription_app/features/subscription/presentation/premium_screen.dart';
 
 class MyApp extends StatelessWidget {
@@ -12,22 +16,59 @@ class MyApp extends StatelessWidget {
   final _router = GoRouter(
     initialLocation: "/login",
     routes: [
+
+      // LOGIN
       GoRoute(
         path: "/login",
         builder: (context, state) => const LoginScreen(),
       ),
+
+      // DASHBOARD
       GoRoute(
         path: "/dashboard",
         builder: (context, state) => AppLayout(
           child: const DashboardScreen(),
         ),
       ),
+
+      // CREATE PROFILE
+      GoRoute(
+        path: "/create-profile",
+        builder: (context, state) =>
+            const CreateProfileScreen(),
+      ),
+
+      // CHAT LIST
       GoRoute(
         path: "/chat",
         builder: (context, state) => AppLayout(
-          child: const ChatScreen(),
+          child: const ChatListScreen(),
         ),
       ),
+
+      // CHAT CONVERSATION (dynamic)
+      GoRoute(
+        path: "/chat/:id",
+        builder: (context, state) {
+          final id = state.pathParameters["id"]!;
+          return ChatConversationScreen(
+            chatUserId: id,
+          );
+        },
+      ),
+
+      // PROFILE DETAILS (dynamic)
+      GoRoute(
+        path: "/profile/:id",
+        builder: (context, state) {
+          final id = state.pathParameters["id"]!;
+          return ProfileDetailsScreen(
+            userId: id,
+          );
+        },
+      ),
+
+      // PREMIUM
       GoRoute(
         path: "/premium",
         builder: (context, state) => AppLayout(
