76569a1f59b1bf1e5a2570dea6f97eda524d0f70
Amitsaka1 <111147203+Amitsaka1@users.noreply.github.com>
2026-02-22 23:53:12 +0530

    Add files via upload

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,48 @@
-q
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:secure_subscription_app/shared/layout/app_layout.dart';
+import 'package:secure_subscription_app/features/auth/presentation/login_screen.dart';
+import 'package:secure_subscription_app/features/chat/presentation/chat_screen.dart';
+import 'package:secure_subscription_app/features/dashboard/presentation/dashboard_screen.dart';
+import 'package:secure_subscription_app/features/subscription/presentation/premium_screen.dart';
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  final _router = GoRouter(
+    initialLocation: "/login",
+    routes: [
+      GoRoute(
+        path: "/login",
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: "/dashboard",
+        builder: (context, state) => AppLayout(
+          child: const DashboardScreen(),
+        ),
+      ),
+      GoRoute(
+        path: "/chat",
+        builder: (context, state) => AppLayout(
+          child: const ChatScreen(),
+        ),
+      ),
+      GoRoute(
+        path: "/premium",
+        builder: (context, state) => AppLayout(
+          child: const PremiumScreen(),
+        ),
+      ),
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: _router,
+      theme: ThemeData.dark(),
+    );
+  }
+}
