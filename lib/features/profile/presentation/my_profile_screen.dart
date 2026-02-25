24ff2750ffdbe6c775e69277204be34e422fd2ce
Amitsaka1 <111147203+Amitsaka1@users.noreply.github.com>
2026-02-25 09:28:32 +0530

    Update my_profile_screen.dart

--- lib/features/profile/presentation/my_profile_screen.dart
+++ lib/features/profile/presentation/my_profile_screen.dart
@@ -12,7 +12,7 @@ class MyProfileScreen extends StatefulWidget {
 class _MyProfileScreenState extends State<MyProfileScreen> {
 
   Map<String, dynamic>? profile;
-  bool loading = false;
+  bool loading = true;
 
   @override
   void initState() {
