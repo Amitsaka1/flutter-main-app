4c1c116464b1ced49978fd232cd27ac6e9df4ca3
Amitsaka1 <111147203+Amitsaka1@users.noreply.github.com>
2026-03-01 01:42:20 +0530

    Create user_session.dart

---
+++ lib/core/session/user_session.dart
@@ -0,0 +1,15 @@
+class UserSession {
+  static String? userId;
+
+  static void setUserId(String id) {
+    userId = id;
+  }
+
+  static String? getUserId() {
+    return userId;
+  }
+
+  static void clear() {
+    userId = null;
+  }
+}
