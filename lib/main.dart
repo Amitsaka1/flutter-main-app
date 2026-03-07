11bb10116252a6c07c317403588848eee8accaec
Amitsaka1 <111147203+Amitsaka1@users.noreply.github.com>
2026-03-07 17:17:24 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'app.dart';
-import 'package:app_project/core/network/api_client.dart';
-import 'package:app_project/core/socket/global_socket_manager.dart';
 
 /// 🔥 Global Navigator Key
 final GlobalKey<NavigatorState> appNavigatorKey =
@@ -11,25 +8,5 @@ final GlobalKey<NavigatorState> appNavigatorKey =
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await _initSocket();
-
   runApp(const MyApp());
-}
-
-Future<void> _initSocket() async {
-  final token = await ApiClient.getToken();
-
-  if (token == null) return;
-
-  final payload = jsonDecode(
-    utf8.decode(
-      base64Url.decode(
-        base64Url.normalize(token.split(".")[1]),
-      ),
-    ),
-  );
-
-  final userId = payload["id"];
-
-  await GlobalSocketManager.instance.init(userId);
 }
