43371cfe334f22f28713b8574317594f3b2dda55
Amitsaka1 <111147203+Amitsaka1@users.noreply.github.com>
2026-03-08 04:15:50 +0530

    Update global_socket_manager.dart

--- lib/core/socket/global_socket_manager.dart
+++ lib/core/socket/global_socket_manager.dart
@@ -80,9 +80,10 @@ class GlobalSocketManager with WidgetsBindingObserver {
           UnreadCounterService.increment(senderId);
         }
 
+        ChatController.instance.handleNewMessage(event);
+
       }
 
-      ChatController.instance.handleNewMessage(event);
 
       _messageController.add(event);
     });
