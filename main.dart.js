594dd4d1fd84b839c2cccb0217eb47036957277a
Amitsaka1 <Amitsaka1@users.noreply.github.com>
2026-03-08 09:16:49 +0000

    deploy: bb3e0c14e7b2c4d20f63331eff45c3611028cc11

--- main.dart.js
+++ main.dart.js
@@ -49449,11 +49449,11 @@ if(o.j(p,"INCOMING_CALL"))r.a.afz(a)
 if(o.j(p,"SEAT_MAP_UPDATE"))r.a.x.D(0,a)
 if(o.j(p,"ROOM_CLOSED"))r.a.y.D(0,null)
 if(o.j(p,"NEW_MESSAGE")){s=J.a6(q.h(a,"data"),"senderId")
-if(!J.c(s,r.a.c)){q=$.GD.h(0,s)
-$.GD.m(0,s,(q==null?0:q)+1)
+if(!J.c(s,r.a.c)){o=$.GD.h(0,s)
+$.GD.m(0,s,(o==null?0:o)+1)
 $.GE=$.GE+1
 $.aMj().D(0,A.kb($.GD,t.N,t.S))
-$.aMk().D(0,$.GE)}$.a4O().a_o(a)}r.a.w.D(0,a)},
+$.aMk().D(0,$.GE)}$.a4O().a_o(q.h(a,"data"))}r.a.w.D(0,a)},
 $S:59}
 A.abD.prototype={
 $1(a){this.a.$0()},
