fa3b11bdf59986694e45f3f0132628d12c818352
Amitsaka1 <111147203+Amitsaka1@users.noreply.github.com>
2026-03-14 15:47:25 +0530

    Create bottom_controls.dart

---
+++ lib/features/room/widgets/bottom_controls.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class BottomControls extends StatelessWidget {
+
+  const BottomControls({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Positioned(
+      bottom:30,
+      left:0,
+      right:0,
+      child: Center(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal:30,vertical:12),
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.6),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: const Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+
+              Icon(Icons.card_giftcard,color: Colors.amber),
+
+              SizedBox(width:40),
+
+              Icon(Icons.mic,color: Colors.white),
+
+              SizedBox(width:40),
+
+              Icon(Icons.image,color: Colors.white),
+
+            ],
+          ),
+        ),
+      ),
+    );
+
+  }
+
+}
