0e761b3d3372ff168aab41638f728112e9fc5e67
Amitsaka1 <111147203+Amitsaka1@users.noreply.github.com>
2026-03-21 20:12:30 +0530

    Update voice_room_controller.dart

--- lib/controllers/voice_room_controller.dart
+++ lib/controllers/voice_room_controller.dart
@@ -98,6 +98,11 @@ class VoiceRoomController {
 
         if (type == "TRANSPORT_CONNECTED") {
           AppDebug.log("[VOICE] TRANSPORT CONNECTED");
+
+          // 🔥 FINAL FIX: start mic here
+          startSpeaking().catchError((e) {
+            AppDebug.log("[VOICE] MIC ERROR: $e");
+          });
         }
 
         if (type == "PRODUCER_CREATED") {
