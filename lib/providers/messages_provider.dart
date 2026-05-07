aa1d60947d8801e4f5c6ba6df3f73e9ecef97b0d
Amitsaka1 <111147203+Amitsaka1@users.noreply.github.com>
2026-05-07 21:14:52 +0530

    Create messages_provider.dart

---
+++ lib/providers/messages_provider.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// 🔥 Global Messages Provider
+final messagesProvider =
+    StateProvider<Map<String, List<dynamic>>>((ref) {
+  return {};
+});
