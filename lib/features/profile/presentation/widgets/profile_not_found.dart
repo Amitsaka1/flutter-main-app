2b1735c4af849b50b0c5dd26b9812a3de99d3fbb
Amitsaka1 <111147203+Amitsaka1@users.noreply.github.com>
2026-05-15 00:43:59 +0530

    Create profile_not_found.dart

---
+++ lib/features/profile/presentation/widgets/profile_not_found.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ProfileNotFound extends StatelessWidget {
+  const ProfileNotFound({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    // ================= UI START =================
+
+    return const Center(
+      child: Text(
+        "Profile not found",
+      ),
+    );
+
+    // ================= UI END =================
+  }
+}
