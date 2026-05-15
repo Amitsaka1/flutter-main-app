b44a637e2ac04bcf2b27c0d4277eb7bbb10ccc4f
Amitsaka1 <111147203+Amitsaka1@users.noreply.github.com>
2026-05-15 18:23:17 +0530

    Create profile_pill_stat.dart

---
+++ lib/features/profile_details/presentation/widgets/profile_pill_stat.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class ProfilePillStat extends StatelessWidget {
+
+  final String title;
+  final String value;
+
+  const ProfilePillStat({
+    super.key,
+    required this.title,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    // ================= UI START =================
+
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 28,
+        vertical: 12,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color: const Color(0xFF1C1F26),
+      ),
+      child: Column(
+        children: [
+
+          Text(
+            value,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+
+          const SizedBox(height: 4),
+
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 12,
+              color: Colors.white54,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    // ================= UI END =================
+  }
+}
