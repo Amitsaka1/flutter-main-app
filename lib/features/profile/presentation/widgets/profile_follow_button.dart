07b71034aa197cade51fca6fe30d7c43a7eb26c4
Amitsaka1 <111147203+Amitsaka1@users.noreply.github.com>
2026-05-15 18:49:23 +0530

    Create profile_follow_button.dart

---
+++ lib/features/profile/presentation/widgets/profile_follow_button.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class ProfileFollowButton extends StatelessWidget {
+  final bool isFollowing;
+  final bool actionLoading;
+  final VoidCallback onTap;
+
+  const ProfileFollowButton({
+    super.key,
+    required this.isFollowing,
+    required this.actionLoading,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // ================= UI START =================
+
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: isFollowing
+              ? const Color(0xFF444B57)
+              : const Color(0xFF1C1F26),
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+        onPressed: actionLoading ? null : onTap,
+        child: actionLoading
+            ? const SizedBox(
+                height: 18,
+                width: 18,
+                child: CircularProgressIndicator(
+                  strokeWidth: 2,
+                  color: Colors.white,
+                ),
+              )
+            : Text(
+                isFollowing ? "Following" : "+ Follow",
+                style: const TextStyle(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+      ),
+    );
+
+    // ================= UI END =================
+  }
+}
