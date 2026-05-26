9f615cbf29d277d2b88cb31e70ba696b4b29761c
Amitsaka1 <111147203+Amitsaka1@users.noreply.github.com>
2026-05-27 00:15:58 +0530

    Update livekit_mobile.dart

--- lib/core/livekit/livekit_mobile.dart
+++ lib/core/livekit/livekit_mobile.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:livekit_client/livekit_client.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
