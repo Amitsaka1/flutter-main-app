eb26ebf91a99292f37d40d10c55f953f062d26a5
Amitsaka1 <Amitsaka1@users.noreply.github.com>
2026-06-04 07:07:19 +0000

    deploy: 4faf9de94421aecf5f56b3ee27c403158aed448e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "218925150"
+    serviceWorkerVersion: "3150685365"
   }
 });
